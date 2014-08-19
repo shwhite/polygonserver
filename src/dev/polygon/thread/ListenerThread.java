18f883b804ac8fbab7e13bc9252ea8991665b9cd
wwhite103 <wwhite103@gmail.com>
2014-08-19 18:39:45 +0900

    /

---
+++ src/dev/polygon/thread/ListenerThread.java
@@ -0,0 +1,20 @@
+package dev.polygon.thread;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class ListenerThread implements Runnable{
+
+	
+	ServerSocketChannel socketchannel  = null;
+	ServerSocket socketSer = null;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
